e903287480d4cc354625d14dd97850383ae4bf68
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2021-02-21 18:16:49 +0300

    added post in movie.routes

--- models/Movie.js
+++ models/Movie.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const MovieSchema = new Schema({
   //directory_id: {type:Schema.Types.ObjectId},
   directory_id: Schema.Types.ObjectId,
@@ -14,7 +15,7 @@ const MovieSchema = new Schema({
   country: { type: String, maxlength: 50, minlength: 3 },
   year: { type: Number, min: 1850, max: 2100 },
   imdb_score: { type: Number, min: 0, max: 10 },
-  createdAt: { type: Date, deafult: Date.now }
+  createdAt: { type: Date, default: Date.now }
 });
 
 
@@ -22,4 +23,4 @@ const MovieSchema = new Schema({
 
 module.exports = mongoose.model('movie', MovieSchema);
 
-//2- Schema oluşturduk ve root oluşturacağız sonra app.js de kullanıma açacağım
+//2- Schema oluşturduk ve root oluşturacağız sonra app.js de kullanıma açacağız
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+//Model
+const MovieModel = require('../models/Movie');
 
 
 //GET
 router.get('/', (req, res) => {
   res.send('GET request to the movie page')
 })
+
+//POST
+router.post('/', function (req, res) {
+  const movie = new MovieModel(req.body);    // Bu yöntem tüm verileri eşleştirir
+
+  movie.save((err, data) => {
+    if (err) res.json(err)
+    res.json(data)
+  });
+})
+
 
 
 
